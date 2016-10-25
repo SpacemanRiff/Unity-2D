59c844afe248c49e3849edb0a555feb4d3c73915
Scott Troutman <troutmansm@gmail.com>
2016-10-25 13:34:03 -0500

    ay

---
+++ Cold Space/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*speed,0), ForceMode2D.Force);
+	}
+}
